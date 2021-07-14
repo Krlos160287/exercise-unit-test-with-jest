c5c965e1cad096dd61828442cd1365f32e27b053
Krlos160287 <martigracia2018@gmail.com>
2021-07-14 19:38:04 +0000

    Comenzando

---
+++ app.js
@@ -0,0 +1,20 @@
+function fromDollarToYen (dollar) {
+    //1 dollar is 110 yen.
+    var dollar = dollar * 110;
+    return dollar;
+}
+
+
+function fromEuroToDollar (euro) {
+    //1 euro is 1.20 dollars.
+    var euro = euro * 1.2;
+    return (euro) ;
+}
+console.log(fromEuroToDollar(3.5))
+
+
+function fromYanToPound (yan) {
+    //1 yan is 0,11 pounds.
+    var yan = yan * 0.11;
+    return yan;
+}
